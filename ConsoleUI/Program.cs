39008925d0172891b22d86c4c7a841df1f1fff84
Archie Fan <archiefan@gmail.com>
2022-10-17 10:44:04 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new CEO();
+
+            accountingVP.FirstName = "Guizhen";
+            accountingVP.LastName = "Liu";
+            accountingVP.CalculatePerHourRate(4);
+
+            IManaged emp = new Manager();
+
+            emp.FirstName = "Archie";
+            emp.LastName = "Fan";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
